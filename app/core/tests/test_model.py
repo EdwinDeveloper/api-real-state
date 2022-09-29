7f49c4f3d96059f4ebd968dc3e7c331e617a81a5
Edwin Giovanni <edwingiovanni@MacBook-Pro-de-Edwin.local>
2022-09-28 22:04:54 -0500

    serialized email user

---
+++ app/core/tests/test_model.py
@@ -0,0 +1,34 @@
+"""
+    Test for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ["test1@CORREO.COM", "test1@correo.com"],
+            ["test2@CORREO.com", "test2@correo.com"],
+            ["TEST3@CORREO.COM", "TEST3@correo.com"],
+            ["Test4@Correo.com", "Test4@correo.com"],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
