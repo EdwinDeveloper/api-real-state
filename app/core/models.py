9bd959967153834fd4e5e28108ff312d4f575ae9
Edwin Giovanni <edwingiovanni@MacBook-Pro-de-Edwin.local>
2022-10-01 17:17:45 -0500

    super user complete

--- app/core/models.py
+++ app/core/models.py
@@ -14,16 +14,33 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        if not email:
+            raise ValueError("User mush have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        """Create a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
+    country_code = models.CharField(max_length=3)
+    phone_number = models.CharField(max_length=12)
+    gender = models.CharField(max_length=1)
+    birthday = models.CharField(max_length=25)
+    # investments=#pendiente
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
