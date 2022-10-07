aeb8438fbcb80704b00aeb11c7631432f2a16708
Edwin Giovanni <eperez@cxclatam.com>
2022-10-07 02:05:21 -0500

    complete recipe

---
+++ app/recipe/urls.py
@@ -0,0 +1,22 @@
+"""
+URL mapping for the recipe app.
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()  # We create a default router
+router.register('recipes', views.RecipeViewSets)
+# Above we create a new endpoint call recipes
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
