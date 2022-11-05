56b2db3f9340be7c4116a0dd3bd49cddb739e0dc
Edwin Giovanni <eperez@cxclatam.com>
2022-11-05 13:48:47 -0600

    image upload implementation

--- Dockerfile
+++ Dockerfile
@@ -14,11 +14,11 @@ RUN python -m venv /py && \
     #Create a new virtual environment that our system will use
     /py/bin/pip install --upgrade pip && \
     #We upgrade the packages of pip dependencies
-    apk add --update --no-cache postgresql-client && \
-    #We install the postgresql client in our container
+    apk add --update --no-cache postgresql-client jpeg-dev && \
+    #We install the postgresql client in our container and the jpeg-dev library
     apk add --update --no-cache --virtual .tmp-build-deps \
     #We set a group of packages for install
-        build-base postgresql-dev musl-dev && \
+        build-base postgresql-dev musl-dev zlib zlib-dev && \
         #Install the dependencies we list
     /py/bin/pip install -r /tmp/requirements.txt && \ 
     #Install dependencies from requirement file
@@ -29,12 +29,19 @@ RUN python -m venv /py && \
     # Delete temporal folder
     apk del .tmp-build-deps && \
     #Remove the package we already install in previous steps
-
     adduser \
         --disabled-password \
         --no-create-home \
-        django-user
-        #Adding the user
+        django-user && \
+        #Adding the user   
+    mkdir -p /vol/web/media && \
+    # we crete the media volume
+    mkdir -p /vol/web/static && \
+    # we create the static volume
+    chown -R django-user:django-user /vol && \
+    # we change the owner with the user django-user in vol
+    chmod -R 755 /vol
+    # we change the mode of the permissions of the directory
 
 ENV PATH="/py/bin:$PATH"
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,10 @@ Views for the recipes APIs
 from rest_framework import (
     viewsets,
     mixins,
+    status,
 )
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -31,6 +34,8 @@ class RecipeViewSets(viewsets.ModelViewSet):
         """Return the serializer class request"""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -38,41 +43,43 @@ class RecipeViewSets(viewsets.ModelViewSet):
         """Create a new Recipe"""
         serializer.save(user=self.request.user)
 
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
 
-# CRUD mixin implemented
-class TagViewSet(
-    mixins.DestroyModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet
-):
-    """Manage tags in the database"""
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status. HTTP_400_BAD_REQUEST)
+
+
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Base viewset for recipe attributes"""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Filter queryset to authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+# CRUD mixin implemented
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
 
     def perform_create(self, serializer):
         """Create a new Recipe"""
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(
-    mixins.DestroyModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet
-):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
