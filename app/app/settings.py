53e9070bc8feedf13c07589aa8f52d04713d0909
Edwin Giovanni <edwindeveloper@outlook.com>
2023-05-02 10:07:41 -0600

    fix gmail

--- app/app/settings.py
+++ app/app/settings.py
@@ -107,7 +107,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'lafamiliaesgrande@gmail.com'
-EMAIL_HOST_PASSWORD = 'jrtqltdjbxlcfnak'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
 # Password validation
